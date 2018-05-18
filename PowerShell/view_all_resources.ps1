f19c6ecae858c966e8964b2ab0f608ccf91d6a2f
Patrick Gryczka <patrickgryczka@outlook.com>
2018-05-18 11:32:48 -0400

    Added a small login check and prompt to the start of non login scripts, so that if they are used when not logged in, the user will automatically be prompted to login, and their workflow should continue, rather than having to specially call the login script.

--- PowerShell/view_all_resources.ps1
+++ PowerShell/view_all_resources.ps1
@@ -1,5 +1,14 @@
 #Trivial script to get all resource groups listed with regions
 ##Source: John Savill
 ###http://www.itprotoday.com/microsoft-azure/savills-faqs-view-your-azure-resources-using-powershell
-echo "Retrieving resource groups and their regions"
+echo "Confirming that you are logged in and returning the current Context"                          #Checking Context in order to confirm if user is logged in (login check from https://stackoverflow.com/a/42229650)
+Try {
+    Get-AzureRmContext
+}
+Catch{                                                                                              #If user is not logged in, welp, we prompt them to log in
+    if ($_ -like "*Login-AzureRmAccount to login*"){
+        Login-AzureRmAccount
+    }
+}
+echo "Retrieving resources with name, type, and location"                                           #Getting the users resources
 Get-AzureRmResource | ft Name, ResourceType, Location -AutoSize
